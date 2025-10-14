520e25cf65e26b1fb7fdcf80f9d9840727704c23
Brandon <111472416+Brandon-J-Navarro@users.noreply.github.com>
2025-10-14 14:11:45 -0400

    renamed / fixed typo

--- EmailLibary/EmailLibrary/Properties/AssemblyInfo.cs
+++ EmailLibary/EmailLibrary/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("EmailLibary")]
+[assembly: AssemblyTitle("EmailLibrary")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("EmailLibary")]
+[assembly: AssemblyProduct("EmailLibrary")]
 [assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
