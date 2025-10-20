180e0e0a6c8c7cc233f12ff15866a3415671db28
Brandon <111472416+Brandon-J-Navarro@users.noreply.github.com>
2025-10-20 09:56:16 -0400

    moved banner to initialization script

--- EmailModule/EmailModule.Libraries.ps1
+++ EmailModule/EmailModule.Libraries.ps1
@@ -15,3 +15,19 @@ if ($PSEdition -eq 'Core') {
         }
     }
 }
+
+function Get-Banner{
+    write-host -fore DarkGreen "    ____           _ __  __  ___        __     __    "
+    write-host -fore DarkGreen "   / __/_ _  ___ _(_/ / /  |/  /__  ___/ /_ __/ /__  "
+    write-host -fore DarkGreen "  / _//  ' \/ _ '/ / / / /|_/ / _ \/ _  / // / / -_) "
+    write-host -fore DarkGreen " /___/_/_/_/\_,_/_/_/ /_/  /_/\___/\_,_/\_,_/_/\__/  "
+
+    write-host -no "Cmdlets available:"
+    write-host -no " "
+    write-host -fore Yellow "Send-Email"
+
+    write-host -no "Get help for Send-Email cmdlet:"
+    write-host -no " "
+    write-host -fore Yellow "Get-Help Send-Email or Send-Email -?"
+}
+Get-Banner
