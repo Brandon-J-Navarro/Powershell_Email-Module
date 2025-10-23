5279fd2fb43a5b08dc36d55aa27aa47cc8550fdd
Brandon <111472416+Brandon-J-Navarro@users.noreply.github.com>
2025-10-23 15:31:19 -0400

    Updated variable Names, Updated some variable Types to Objects, added additional Parameters, and added Cc, Bcc, and Attachment sections

--- EmailLibraryCore/EmailLibrary/EmailLibrary.cs
+++ EmailLibraryCore/EmailLibrary/EmailLibrary.cs
@@ -1,39 +1,103 @@
 ﻿// EmailLibrary.cs
-using MimeKit;
 using MailKit.Security;
+using MimeKit;
+using Microsoft.AspNetCore.StaticFiles;
 public class EmailCommands
 {
     public static void SendEmail(
-        string _AuthUser,
-        string _AuthPass,
-        string _EmailTo,
-        string? _EmailToName,
-        string _EmailFrom,
-        string? _EmailFromName,
-        string? _EmailSubject,
-        string? _EmailBody,
-        string _EmailMailServer,
-        int _EmailServerPort)
+        string authUser,
+        string authPass,
+        object[] emailTo,
+        object[]? toName,
+        object[]? emailCc,
+        object[]? ccName,
+        object[]? emailBcc,
+        object[]? bccName,
+        string? emailAttachment,
+        string emailFrom,
+        string? fromName,
+        string? emailSubject,
+        string? emailBody,
+        string mailServer,
+        int serverPort)
     {
         var mailMessage = new MimeMessage();
-        mailMessage.From.Add(string.IsNullOrEmpty(_EmailFromName)
-            ? new MailboxAddress(_EmailFrom, _EmailFrom)
-            : new MailboxAddress(_EmailFromName, _EmailFrom));
+        mailMessage.From.Add(string.IsNullOrEmpty(fromName)
+            ? new MailboxAddress(emailFrom, emailFrom)
+            : new MailboxAddress(fromName, emailFrom));
 
-        mailMessage.To.Add(string.IsNullOrEmpty(_EmailToName)
-            ? new MailboxAddress(_EmailTo, _EmailTo)
-            : new MailboxAddress(_EmailToName, _EmailTo));
-
-        mailMessage.Subject = _EmailSubject ?? string.Empty;
-
-        mailMessage.Body = new TextPart("plain")
+        for (int i = 0; i < emailTo.Length; i++)
         {
-            Text = _EmailBody ?? string.Empty
-        };
+            mailMessage.To.Add(string.IsNullOrEmpty(toName[i].ToString())
+                ? new MailboxAddress(emailTo[i].ToString(), emailTo[i].ToString())
+                : new MailboxAddress(toName[i].ToString(), emailTo[i].ToString()));
+        }
+
+        if (emailCc != null)
+        {
+            for (int i = 0; i < emailCc.Length; i++)
+            {
+                mailMessage.Cc.Add(string.IsNullOrEmpty(ccName[i].ToString())
+                    ? new MailboxAddress(emailCc[i].ToString(), emailCc[i].ToString())
+                    : new MailboxAddress(ccName[i].ToString(), emailCc[i].ToString()));
+            }
+        }
+
+        if (emailBcc != null)
+        {
+            for (int i = 0; i < emailTo.Length; i++)
+            {
+                mailMessage.Bcc.Add(string.IsNullOrEmpty(bccName[i].ToString())
+                    ? new MailboxAddress(emailBcc[i].ToString(), emailBcc[i].ToString())
+                    : new MailboxAddress(bccName[i].ToString(), emailBcc[i].ToString()));
+            }
+        }
+
+        mailMessage.Subject = emailSubject ?? string.Empty;
+
+        if (emailAttachment != null)
+        {
+            var body = new TextPart("plain")
+            {
+                Text = emailBody ?? string.Empty
+            };
+
+            // create an image attachment for the file located at path
+            const string DefaultContentType = "application/octet-stream";
+            var provider = new FileExtensionContentTypeProvider();
+            if (!provider.TryGetContentType(emailAttachment, out string contentType))
+            {
+                contentType = DefaultContentType;
+            }
+
+            var attachment = new MimePart(contentType)
+            {
+                Content = new MimeContent(File.OpenRead(emailAttachment), ContentEncoding.Default),
+                ContentDisposition = new ContentDisposition(ContentDisposition.Attachment),
+                ContentTransferEncoding = ContentEncoding.Base64,
+                FileName = Path.GetFileName(emailAttachment)
+            };
+
+            // now create the multipart/mixed container to hold the message text and the
+            // image attachment
+            var multipart = new Multipart("mixed");
+            multipart.Add(body);
+            multipart.Add(attachment);
+
+            // now set the multipart/mixed as the message body
+            mailMessage.Body = multipart;
+        }
+        else
+        {
+            mailMessage.Body = new TextPart("plain")
+            {
+                Text = emailBody ?? string.Empty
+            };
+        }
 
         using var smtpClient = new MailKit.Net.Smtp.SmtpClient();
-        smtpClient.Connect(_EmailMailServer, _EmailServerPort, SecureSocketOptions.StartTls);
-        smtpClient.Authenticate(_AuthUser, _AuthPass);
+        smtpClient.Connect(mailServer, serverPort, SecureSocketOptions.StartTls);
+        smtpClient.Authenticate(authUser, authPass);
         smtpClient.Send(mailMessage);
         smtpClient.Disconnect(true);
     }
