de98ab01a36036140742e0c6b03d8fd2146bbc47
Brandon <111472416+Brandon-J-Navarro@users.noreply.github.com>
2025-10-24 13:40:04 -0400

    added new parameters priority and importance, added the ability to have secure string passed in along with plain text string and moved mail message building to individual methods

--- EmailLibraryCore/EmailLibrary/EmailLibrary.cs
+++ EmailLibraryCore/EmailLibrary/EmailLibrary.cs
@@ -1,74 +1,44 @@
 ﻿// EmailLibrary.cs
 using MailKit.Security;
-using MimeKit;
 using Microsoft.AspNetCore.StaticFiles;
+using MimeKit;
+using System.Net;
+using System.Net.Mail;
+using System.Security;
 public class EmailCommands
 {
     public static void SendEmail(
-        string authUser, string authPass,
+        string authUser, object authPass,
         string emailTo, string? toName,
         string emailFrom, string? fromName,
         string? emailSubject, string? emailBody,
         string mailServer, int serverPort,
         string? emailCc, string? ccName,
         string? emailBcc, string? bccName,
-        string? emailAttachment)
+        string? emailAttachment, string? emailPriority,
+        string? emailImportance)
     {
+        NetworkCredential credentials = CreateAuthCreds(authUser, authPass);
         var mailMessage = new MimeMessage();
-        mailMessage.From.Add(string.IsNullOrEmpty(fromName)
-            ? new MailboxAddress(emailFrom, emailFrom)
-            : new MailboxAddress(fromName, emailFrom));
-
-        var EmailRecipientTo = emailTo.Split(';');
-        var EmailRecipientToName = toName.Split(';');
-        for (int i = 0; i < EmailRecipientTo.Length; i++)
-        {
-            if (EmailRecipientToName.Length < EmailRecipientTo.Length || EmailRecipientToName.Length > EmailRecipientTo.Length)
-            {
-                mailMessage.To.Add(new MailboxAddress(EmailRecipientTo[i], EmailRecipientTo[i]));
-            }
-            else if (EmailRecipientToName.Length == EmailRecipientTo.Length)
-            {
-                mailMessage.To.Add(new MailboxAddress(EmailRecipientToName[i], EmailRecipientTo[i]));
-            }
-        }
-
+        mailMessage = BuildMailMessageFrom(mailMessage, emailFrom, fromName);
+        mailMessage = BuildMailMessageTo(mailMessage, emailTo, toName);
         if (emailCc != null)
         {
-            var EmailRecipientCc = emailCc.Split(";");
-            var EmailRecipientCcName = ccName.Split(';');
-            for (int i = 0; i < EmailRecipientCc.Length; i++)
-            {
-                if (EmailRecipientCcName.Length < EmailRecipientCc.Length || EmailRecipientCcName.Length > EmailRecipientCc.Length)
-                {
-                    mailMessage.Cc.Add(new MailboxAddress(EmailRecipientCc[i], EmailRecipientCc[i]));
-                }
-                else if (EmailRecipientCcName.Length == EmailRecipientCc.Length)
-                {
-                    mailMessage.Cc.Add(new MailboxAddress(EmailRecipientCcName[i], EmailRecipientCc[i]));
-                }
-            }
+            mailMessage = BuildMailMessageCc(mailMessage, emailCc, ccName);
         }
-
         if (emailBcc != null)
         {
-            var EmailRecipientBcc = emailBcc.Split(";");
-            var EmailRecipientBccName = bccName.Split(';');
-            for (int i = 0; i < EmailRecipientBcc.Length; i++)
-            {
-                if (EmailRecipientBccName.Length < EmailRecipientBcc.Length || EmailRecipientBccName.Length > EmailRecipientBcc.Length)
-                {
-                    mailMessage.Bcc.Add(new MailboxAddress(EmailRecipientBcc[i], EmailRecipientBcc[i]));
-                }
-                else if (EmailRecipientBccName.Length == EmailRecipientBcc.Length)
-                {
-                    mailMessage.Bcc.Add(new MailboxAddress(EmailRecipientBccName[i], EmailRecipientBcc[i]));
-                }
-            }
+            mailMessage = BuildMailMessageBcc(mailMessage, emailBcc, bccName);
         }
-
+        if (emailPriority != null)
+        {
+            mailMessage.Priority = (MessagePriority)System.Enum.Parse(typeof(MessagePriority), emailPriority);
+        }
+        if (emailImportance != null)
+        {
+            mailMessage.Importance = (MessageImportance)System.Enum.Parse(typeof(MessagePriority), emailImportance);
+        }
         mailMessage.Subject = emailSubject ?? string.Empty;
-
         if (emailAttachment != null)
         {
             //    var body = new TextPart("plain")
@@ -116,8 +86,87 @@ public class EmailCommands
 
         using var smtpClient = new MailKit.Net.Smtp.SmtpClient();
         smtpClient.Connect(mailServer, serverPort, SecureSocketOptions.StartTls);
-        smtpClient.Authenticate(authUser, authPass);
+        smtpClient.Authenticate(credentials);
         smtpClient.Send(mailMessage);
         smtpClient.Disconnect(true);
+    }
+
+    static private NetworkCredential CreateAuthCreds(string userName, object password)
+    {
+        NetworkCredential credentials;
+        switch (password)
+        {
+            case string stringPassword:
+                credentials = new NetworkCredential(userName, stringPassword);
+                break;
+            case SecureString securePassword:
+                credentials = new NetworkCredential(userName, securePassword);
+                break;
+            default:
+                throw new ArgumentException("Password must be either string or SecureString", nameof(password));
+        }
+        return credentials;
+    }
+
+    static private MimeMessage BuildMailMessageFrom(MimeMessage mailMessage, string emailFrom, string? fromName)
+    {
+        mailMessage.From.Add(string.IsNullOrEmpty(fromName)
+            ? new MailboxAddress(emailFrom, emailFrom)
+            : new MailboxAddress(fromName, emailFrom));
+        return mailMessage;
+    }
+
+    static private MimeMessage BuildMailMessageTo(MimeMessage mailMessage, string emailTo, string? toName)
+    {
+        var EmailRecipientTo = emailTo.Split(';');
+        var EmailRecipientToName = toName.Split(';');
+        for (int i = 0; i < EmailRecipientTo.Length; i++)
+        {
+            if (EmailRecipientToName.Length < EmailRecipientTo.Length || EmailRecipientToName.Length > EmailRecipientTo.Length)
+            {
+                mailMessage.To.Add(new MailboxAddress(EmailRecipientTo[i], EmailRecipientTo[i]));
+            }
+            else if (EmailRecipientToName.Length == EmailRecipientTo.Length)
+            {
+                mailMessage.To.Add(new MailboxAddress(EmailRecipientToName[i], EmailRecipientTo[i]));
+            }
+        }
+        return mailMessage;
+    }
+
+    static private MimeMessage BuildMailMessageCc(MimeMessage mailMessage,string emailCc, string? ccName)
+    {
+        var EmailRecipientCc = emailCc.Split(";");
+        var EmailRecipientCcName = ccName.Split(';');
+        for (int i = 0; i < EmailRecipientCc.Length; i++)
+        {
+            if (EmailRecipientCcName.Length < EmailRecipientCc.Length || EmailRecipientCcName.Length > EmailRecipientCc.Length)
+            {
+                mailMessage.Cc.Add(new MailboxAddress(EmailRecipientCc[i], EmailRecipientCc[i]));
+            }
+            else if (EmailRecipientCcName.Length == EmailRecipientCc.Length)
+            {
+                mailMessage.Cc.Add(new MailboxAddress(EmailRecipientCcName[i], EmailRecipientCc[i]));
+            }
+        }
+        return mailMessage;
+    }
+
+    static private MimeMessage BuildMailMessageBcc(MimeMessage mailMessage, string emailBcc, string? bccName)
+    {
+        var EmailRecipientCc = emailBcc.Split(";");
+        var EmailRecipientCcName = bccName.Split(';');
+        for (int i = 0; i < EmailRecipientCc.Length; i++)
+        {
+            if (EmailRecipientCcName.Length < EmailRecipientCc.Length || EmailRecipientCcName.Length > EmailRecipientCc.Length)
+            {
+                mailMessage.Cc.Add(new MailboxAddress(EmailRecipientCc[i], EmailRecipientCc[i]));
+            }
+            else if (EmailRecipientCcName.Length == EmailRecipientCc.Length)
+            {
+                mailMessage.Cc.Add(new MailboxAddress(EmailRecipientCcName[i], EmailRecipientCc[i]));
+            }
+        }
+        return mailMessage;
     }
 }
