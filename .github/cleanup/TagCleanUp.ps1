fc57980c650798b8ca7bbc20f22b086c8e3266f2
Brandon <111472416+Brandon-J-Navarro@users.noreply.github.com>
2025-10-29 14:48:48 -0400

    updated clean up script

--- .github/cleanup/TagCleanUp.ps1
+++ .github/cleanup/TagCleanUp.ps1
@@ -2,9 +2,9 @@
 
 $tags = git tag --list
 
-$workingTags = $tags | Where-Object { $_ -like "*working*" }
+$workingTags = $tags | Where-Object { $_ -like "*working*" -or $_ -like "*development*"-or $_ -like "*testing*"}
 
-if ( 0 -lt $workingTags.count ) {
+if ( 0 -le $workingTags.count ) {
     $workingTags | ForEach-Object {
         git tag -d $_
     }
